4ac760cda01a0021e59965760dc65ffe7e63f37e
Mohemmad Imtiyaz <imtiyajkhan789@gmail.com>
2021-11-03 13:18:37 +0530

    created model, controller and routes for post messages. And connect our database to server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose  from 'mongoose';
+
+// importing our routes here
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({limit: '30mb', extended : true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended : true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://imtiyajkhan789:11020170@cluster0.uzmfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser : true, useUnifiedTopology : true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on Port : ${PORT}`)))
+    .catch((e) => console.log(e.message));
+
+// mongoose.set("useFindAndModify", false);
